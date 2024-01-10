1a32f4b19be6bcb0cf985fb4811adbeca30a9246
rom-vtn <romahvito@gmail.com>
2024-01-10 08:59:22 +0100

    add Codeopolis class

---
+++ src/main/java/fr/romaneviton/Codeopolis.java
@@ -0,0 +1,10 @@
+package fr.romaneviton;
+
+public class Codeopolis {
+    public static void main(String[] args) {
+        City city = new City(null);
+        TextInterface textInterface = new TextInterface();
+
+        int acrePrice = 40; //TODO: make prices variable later
+    }
+}
