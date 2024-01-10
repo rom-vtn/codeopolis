dc9b0a440e265afe2cde3fc806112ffb6ec316ce
rom-vtn <romahvito@gmail.com>
2024-01-10 08:55:31 +0100

    rename to conform to spec

--- src/main/java/fr/romaneviton/TextInterface.java
+++ src/main/java/fr/romaneviton/TextInterface.java
@@ -1,7 +1,7 @@
 package fr.romaneviton;
 import java.util.Scanner;
 
-public class Menu {
+public class TextInterface {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
