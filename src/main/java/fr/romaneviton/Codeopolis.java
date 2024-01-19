3f315becd8a45ace7c1bebc54d9a4d56fee6f6b0
rom-vtn <romahvito@gmail.com>
2024-01-19 12:50:26 +0100

    change structure to implement end of inc3

--- src/main/java/fr/romaneviton/Codeopolis.java
+++ src/main/java/fr/romaneviton/Codeopolis.java
@@ -1,68 +1,11 @@
 package fr.romaneviton;
 
-import dev.codeopolis.increment03.TurnResult;
-
 public class Codeopolis {
     public static void main(String[] args) {
-        int ACRE_PRICE = 40; //TODO: make prices variable later
         TextInterface textInterface = new TextInterface();
-        City city; 
-        TurnResult turnResult;
-        boolean gameWon = false;
+        Game game;
         while (textInterface.mainMenu() != TextInterface.MainMenuOptions.QUIT) {
-            // Main menu
-            // TextInterface.GameMenuOptions gameMenuOption = textInterface.gameMenu();
-            // while (gameMenuOption != TextInterface.GameMenuOptions.QUIT) {
-            //     switch (gameMenuOption) {
-            //         case BUY:
-            //             textInterface.buyMenu(city, ACRE_PRICE);
-            //             break;
-            //         case SELL:
-            //             textInterface.sellMenu(city, ACRE_PRICE);
-            //             break;
-            //         case FEED:
-            //             textInterface.feedMenu(city);
-            //             break;
-            //         case PLANT:
-            //             textInterface.plantMenu(city);
-            //             break;
-            //         case SHOW_STATUS:
-            //             textInterface.showStatus(city);
-            //             break; 
-            //         default:
-            //             //this should never be run
-            //             break;
-            //     }
-            //     gameMenuOption = textInterface.gameMenu();
-            // }
-            city = new City(textInterface.askCityName("What's that name of your city? "));
-            while (true) { //TODO: add quit condition or death condition
-                // do interface stuff
-                textInterface.showStatus(city);
-                textInterface.buyMenu(city, ACRE_PRICE);
-                textInterface.sellMenu(city, ACRE_PRICE);
-                textInterface.feedMenu(city);
-                textInterface.plantMenu(city);
-    
-                // run turn and get results
-                turnResult = city.runTurn();
-    
-                //show new status
-                textInterface.showStatus(city);
-    
-                //check if win conds apply
-                if (turnResult.getYear() == 10) {
-                    gameWon = true;
-                    break;
-                }
-
-                //check if lose conds apply
-                if (turnResult.getStarvedPercentage() > 50 || turnResult.getResidents() == 0) {
-                    gameWon = false;
-                    break;
-                }
-            }
-            System.out.println("Game won? ---> " + gameWon);
+            game = new Game(textInterface);
         }
         textInterface.close(); //close the scanner inside
     }
---
+++ src/main/java/fr/romaneviton/Game.java
@@ -0,0 +1,48 @@
+package fr.romaneviton;
+
+import dev.codeopolis.increment03.TurnResult;
+
+public class Game {
+    public static final int ACRE_PRICE = 40;
+    private TextInterface parent;
+    private City city;
+
+    private void runGame() {
+        int ACRE_PRICE = 40; //TODO: make prices variable later
+        TurnResult turnResult;
+        boolean gameWon = false;
+        city = new City(parent.askCityName("What's that name of your city? "));
+        while (true) { //TODO: add quit condition or death condition
+            // do interface stuff
+            parent.showStatus(city);
+            parent.buyMenu(city, ACRE_PRICE);
+            parent.sellMenu(city, ACRE_PRICE);
+            parent.feedMenu(city);
+            parent.plantMenu(city);
+
+            // run turn and get results
+            turnResult = city.runTurn();
+
+            //show new status
+            parent.showStatus(city);
+
+            //check if win conds apply
+            if (turnResult.getYear() == 10) {
+                gameWon = true;
+                break;
+            }
+
+            //check if lose conds apply
+            if (turnResult.getStarvedPercentage() > 50 || turnResult.getResidents() == 0) {
+                gameWon = false;
+                break;
+            }
+        }
+        System.out.println("Game won? ---> " + gameWon);
+    }
+
+    public Game(TextInterface parent) {
+        this.parent = parent;
+        runGame();
+    }
+}
