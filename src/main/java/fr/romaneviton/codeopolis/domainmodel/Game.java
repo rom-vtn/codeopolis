2f684f7ad9f28e6802819546331e0e30a24a6a13
rom-vtn <romahvito@gmail.com>
2024-01-24 09:20:14 +0100

    move game logic form TextInterface to Game

--- src/main/java/fr/romaneviton/codeopolis/domainmodel/Game.java
+++ src/main/java/fr/romaneviton/codeopolis/domainmodel/Game.java
@@ -3,7 +3,7 @@ package fr.romaneviton.codeopolis.domainmodel;
 import java.util.Random;
 
 public class Game {
-    private UserInterface parent;
+    private UserInterface userInterface;
     private City city;
     private GameConfig gameConfig;
     private Random random;
@@ -11,20 +11,61 @@ public class Game {
     private void runGame() {
         TurnResult turnResult;
         boolean gameWon = false;
+        int inputValue;
         while (true) {
             // do interface stuff
             int acrePrice = random.nextInt(gameConfig.getMaxAcrePrice()-gameConfig.getMinAcrePrice()) + gameConfig.getMinAcrePrice();
-            parent.showStatus(city);
-            parent.buyMenu(city, acrePrice);
-            parent.sellMenu(city, acrePrice);
-            parent.feedMenu(city);
-            parent.plantMenu(city);
+            //userInterface.showStatus(city);
+            
+            // userInterface.buyMenu(city, acrePrice);
+            while (true) {
+                inputValue = userInterface.buy(acrePrice, city);
+                if (city.buy(inputValue, acrePrice)) {
+                    break;
+                } else {
+                    userInterface.illegalInput("Can't buy this!");
+                }
+            }
+
+            //userInterface.sellMenu(city, acrePrice);
+            while (true) {
+                inputValue = userInterface.sell(acrePrice, city);
+                if (city.sell(inputValue, acrePrice)) {
+                    break;
+                } else {
+                    userInterface.illegalInput("Can't sell this!");
+                }
+            }
+            
+            //userInterface.feedMenu(city);
+            while (true) {
+                //NOTE: spec says ui.feed must return the number of *residents* fed, not the bushels
+                //FIXME change TextInterface's return value accordingly
+                inputValue = userInterface.feed(gameConfig.getBushelsPerResident(), city);
+                //multiply to fix this
+                if (city.feed(inputValue * gameConfig.getBushelsPerResident())) {
+                    break;
+                } else {
+                    userInterface.illegalInput("Can't feed this!");
+                }
+            }
+            
+            //userInterface.plantMenu(city);
+            while (true) {
+                inputValue = userInterface.plant(gameConfig.getBushelsPerAcre(), gameConfig.getBushelsPerResident(), city);
+                if (city.plant(inputValue)) {
+                    break;
+                } else {
+                    userInterface.illegalInput("Can't plant this!");
+                }
+            }
 
             // run turn and get results
             turnResult = city.runTurn();
 
             //show new status
-            parent.showStatus(city);
+            //userInterface.showStatus(city);
+            userInterface.turnEnd(turnResult);
 
             //check if win conds apply
             if (turnResult.getYear() == gameConfig.getNumberOfYears()) {
@@ -38,13 +79,20 @@ public class Game {
                 break;
             }
         }
-        System.out.println("Game won? ---> " + gameWon);
+        if (gameWon) {
+            userInterface.gameWon("Congration! You done it");
+        } else {
+            userInterface.gameLost("Looks like our monarch did a fatal oopsie and got dethroned");
+        }
     }
 
     public Game(UserInterface parent) {
-        this.parent = parent;
-        gameConfig = new GameConfig(parent.askDifficultyLevel());
-        city = new City(parent.askCityName("What's that name of your city? "), gameConfig);
+        this.userInterface = parent;
+        //gameConfig = new GameConfig(parent.askDifficultyLevel());
+        //city = new City(parent.askCityName("What's that name of your city? "), gameConfig);
+        //TODO actually ask difficulty level and city name once we're done debugging
+        gameConfig = new GameConfig(DifficultyLevel.MEDIUM);
+        city = new City("Codeopolis", gameConfig);
         random = new Random();
         runGame();
     }
