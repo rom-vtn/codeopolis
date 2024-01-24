8287724ec33aefda588c0d1bf1d91867e4842578
rom-vtn <romahvito@gmail.com>
2024-01-24 08:27:47 +0100

    fix warnings for unused vars

--- src/main/java/fr/romaneviton/codeopolis/Codeopolis.java
+++ src/main/java/fr/romaneviton/codeopolis/Codeopolis.java
@@ -3,9 +3,8 @@ package fr.romaneviton.codeopolis;
 public class Codeopolis {
     public static void main(String[] args) {
         TextInterface textInterface = new TextInterface();
-        Game game;
         while (textInterface.mainMenu() != TextInterface.MainMenuOptions.QUIT) {
-            game = new Game(textInterface);
+            new Game(textInterface);
         }
         textInterface.close(); //close the scanner inside
     }
--- src/main/java/fr/romaneviton/codeopolis/DifficultyLevel.java
+++ src/main/java/fr/romaneviton/codeopolis/DifficultyLevel.java
@@ -1,9 +1,5 @@
 package fr.romaneviton.codeopolis;
 
 public enum DifficultyLevel {
-    EASY("easy"), MEDIUM("medium"), HARD("hard");
-    private final String s;
-    DifficultyLevel(String s) {
-        this.s = s;
-    }
+    EASY, MEDIUM, HARD;
 }
